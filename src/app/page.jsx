0ca1981f28946a58ba29e6c4b03ffb9fc6d2407c
Misael <misaelborges1981@gmail.com>
2025-10-24 20:03:31 -0300

    Setup inicial - Iniciando um sonho

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Rangooo</h1>
+      <p>
+        Rangooo é um projeto de código aberto que visa fornecer uma interface de
+        usuário intuitiva e personalizável para interações com modelos de
+        linguagem.
+      </p>
+    </>
+  );
+}
