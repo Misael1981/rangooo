5f1d364c9014c5aae650fdd8078baab8236efacd
Misael <misaelborges1981@gmail.com>
2025-11-09 21:28:37 -0300

    Ajustando layout do SheetCart

--- src/app/restaurantes/[slug]/menu/components/SheetCart/index.jsx
+++ src/app/restaurantes/[slug]/menu/components/SheetCart/index.jsx
@@ -9,25 +9,31 @@ import {
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart";
 import CartItem from "../CartItem";
+import { Button } from "@/components/ui/button";
 
-const SheetCart = () => {
+function SheetCart() {
   const { isOpen, toggleCart, products } = useContext(CartContext);
   return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
-      <SheetContent className="w-[90%]">
-        <SheetHeader>
+      <SheetContent className="flex h-full w-[90%] flex-col p-0">
+        <SheetHeader className="p-4">
           <SheetTitle className="text-left">Sacola</SheetTitle>
         </SheetHeader>
-        <section className="mt-6 space-y-2">
-          <div>
-            {products.map((product, index) => (
-              <CartItem key={index} product={product} />
-            ))}
-          </div>
-        </section>
+
+        <div className="min-h-0 flex-1 space-y-4 overflow-y-auto px-4">
+          {products.map((product, index) => (
+            <CartItem key={index} product={product} />
+          ))}
+        </div>
+
+        <div className="border-t border-gray-300 bg-white p-4">
+          <Button className="h-10 w-full" variant="default">
+            Finalizar pedido
+          </Button>
+        </div>
       </SheetContent>
     </Sheet>
   );
-};
+}
 
 export default SheetCart;
