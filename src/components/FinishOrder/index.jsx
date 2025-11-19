9bcb08a1cf2f208e7db9053a3fe9d6f817926002
Misael <misaelborges1981@gmail.com>
2025-11-19 16:32:27 -0300

    AnimationFadeIn

--- src/components/FinishOrder/index.jsx
+++ src/components/FinishOrder/index.jsx
@@ -16,6 +16,7 @@ import FinishDineIn from "./components/FinishDineIn";
 import { useSearchParams } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
+import AnimationFadeIn from "../AnimationFadeIn";
 
 const FinishOrder = ({ isOpen, onOpenChange }) => {
   const sp = useSearchParams();
@@ -32,42 +33,46 @@ const FinishOrder = ({ isOpen, onOpenChange }) => {
 
   return (
     <Drawer open={isOpen} onOpenChange={onOpenChange}>
-      <DrawerContent className="h-fit">
-        <DrawerHeader className="p-2">
-          <DrawerTitle className="text-2xl font-bold text-red-500">
-            Quase lá!
-          </DrawerTitle>
-          <DrawerDescription>
-            Método de consumo: {consumptionMethod}
-          </DrawerDescription>
-        </DrawerHeader>
-        <section>
-          {consumptionMethod === "DELIVERY" && (
-            <FinishDelivery
-              isLogged={isLogged}
-              onStepChange={setIsFinalStep}
-              externalSubmitTrigger={submitTrigger}
-            />
-          )}
-          {consumptionMethod === "PICKUP" && <FinishPickup />}
-          {consumptionMethod === "DINE_IN" && <FinishDineIn />}
-        </section>
-        <DrawerFooter>
-          {isFinalStep && (
-            <>
-              <Button
-                disabled={!canFinish}
-                onClick={() => setSubmitTrigger((v) => !v)}
-              >
-                Confirmar
-              </Button>
-              <DrawerClose asChild>
-                <Button variant="destructive">Cancelar</Button>
-              </DrawerClose>
-            </>
-          )}
-        </DrawerFooter>
-      </DrawerContent>
+      <AnimationFadeIn>
+        <DrawerContent className="h-fit">
+          <DrawerHeader className="p-2">
+            <DrawerTitle className="text-2xl font-bold text-red-500">
+              Quase lá!
+            </DrawerTitle>
+            <DrawerDescription>
+              Método de consumo: {consumptionMethod}
+            </DrawerDescription>
+          </DrawerHeader>
+          <section>
+            {consumptionMethod === "DELIVERY" && (
+              <FinishDelivery
+                isLogged={isLogged}
+                onStepChange={setIsFinalStep}
+                externalSubmitTrigger={submitTrigger}
+              />
+            )}
+            {consumptionMethod === "PICKUP" && <FinishPickup />}
+            {consumptionMethod === "DINE_IN" && <FinishDineIn />}
+          </section>
+          <AnimationFadeIn>
+            <DrawerFooter>
+              {isFinalStep && (
+                <div className="flex w-full flex-col gap-2">
+                  <Button
+                    disabled={!canFinish}
+                    onClick={() => setSubmitTrigger((v) => !v)}
+                  >
+                    Confirmar
+                  </Button>
+                  <DrawerClose asChild>
+                    <Button variant="destructive">Cancelar</Button>
+                  </DrawerClose>
+                </div>
+              )}
+            </DrawerFooter>
+          </AnimationFadeIn>
+        </DrawerContent>
+      </AnimationFadeIn>
     </Drawer>
   );
 };
