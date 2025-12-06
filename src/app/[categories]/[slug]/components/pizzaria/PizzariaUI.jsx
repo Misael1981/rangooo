527bfa249892b60ad578a88285dbc883dc658720
Misael <misaelborges1981@gmail.com>
2025-12-06 20:04:56 -0300

    Rota pizzaria

---
+++ src/app/[categories]/[slug]/components/pizzaria/PizzariaUI.jsx
@@ -0,0 +1,72 @@
+import ConsumptionMethodOption from "@/components/ConsumptionMethodOption";
+import LogoImage from "@/components/LogoImage";
+import QrCode from "@/components/QrCode";
+import WelcomeSection from "@/components/WelcomeSection";
+
+const METHOD_OPTIONS_MAP = {
+  DELIVERY: {
+    imageUrl: "/images/delivery.png",
+    alt: "Entregar",
+    buttonText: "Entregar",
+  },
+  PICKUP: {
+    imageUrl: "/images/takeaway.png",
+    alt: "Pegar no local",
+    buttonText: "Pegar no local",
+  },
+  DINE_IN: {
+    imageUrl: "/images/dine_in.png",
+    alt: "Comer no local",
+    buttonText: "Comer no local",
+  },
+};
+
+const PizzariaUI = ({ establishment }) => {
+  const slug = establishment.slug;
+  const availableMethods = establishment.consumptionMethods;
+  const segment = "pizzarias";
+
+  return (
+    <div>
+      <div className="relative min-h-screen bg-white sm:py-8">
+        <div className="fixed bottom-8 left-8 hidden lg:block">
+          <QrCode />
+        </div>
+        <div className="mx-auto flex max-w-xl flex-col items-center justify-center gap-4 rounded-lg bg-yellow-50 p-6 shadow-all-sides">
+          <div className="flex flex-col items-center justify-center space-y-2 pt-10">
+            <LogoImage restaurant={establishment} />
+            <h1 className="text-3xl font-bold text-[--brand-primary]">
+              {establishment.name}
+            </h1>
+          </div>
+          <WelcomeSection />
+          <div className="flex flex-col items-center justify-center gap-4 bg-yellow-50">
+            <div className="flex flex-wrap items-center justify-center gap-4">
+              {availableMethods.map((methodObj) => {
+                const methodEnum = methodObj.method;
+                const uiData = METHOD_OPTIONS_MAP[methodEnum];
+                if (!uiData) return null;
+                return (
+                  <ConsumptionMethodOption
+                    key={methodEnum}
+                    imageUrl={uiData.imageUrl}
+                    alt={uiData.alt}
+                    buttonText={uiData.buttonText}
+                    option={methodEnum}
+                    slug={slug}
+                    segment={segment}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="fixed bottom-8 right-8 hidden lg:block">
+          <QrCode />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PizzariaUI;
