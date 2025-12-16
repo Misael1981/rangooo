7883f2200cd9eacb4276d83e4fe4c51102a1cd17
Misael <misaelborges1981@gmail.com>
2025-12-16 18:51:13 -0300

    Corrigindo rota auth

--- src/app/api/admin/methods/route.js
+++ src/app/api/admin/methods/route.js
@@ -3,7 +3,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
+import { authOptions } from "../../auth/[...nextauth]/route";
 
 // PATCH - Atualizar métodos
 export async function PATCH(request) {
