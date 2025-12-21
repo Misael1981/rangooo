b5088649d2701b706eaa79b39c1b92df39e75333
Misael <misaelborges1981@gmail.com>
2025-12-21 18:20:59 -0300

    Erro da vercel

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,17 +1,19 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 const globalForPrisma = globalThis;
 
-const prisma =
+// Declaramos a constante com export direto
+export const prisma =
   globalForPrisma.cachedPrisma ??
   new PrismaClient({
-    log: isProd ? ['error'] : ['warn', 'error'],
+    log: isProd ? ["error"] : ["warn", "error"],
   });
 
 if (!isProd) {
   globalForPrisma.cachedPrisma = prisma;
 }
 
-export default prisma;
+// Mantemos esses para compatibilidade com o que você já escreveu em outros arquivos
 export const db = prisma;
+export default prisma;
