4f5422c54a798eb4127db8427dad2d749d62a038
Misael <misaelborges1981@gmail.com>
2026-01-03 09:35:57 -0300

    Adicionando Suspense

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { CartProvider } from "./contexts/cart";
 import AuthProvider from "./providers/auth";
 import { Toaster } from "@/components/ui/sonner";
+import { Suspense } from "react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -38,7 +39,9 @@ export default function RootLayout({ children }) {
     <html lang="pt-BR">
       <body className={`${inter.className} antialiased`}>
         <AuthProvider>
-          <CartProvider>{children}</CartProvider>
+          <Suspense fallback={null}>
+            <CartProvider>{children}</CartProvider>
+          </Suspense>
           <Toaster />
         </AuthProvider>
       </body>
