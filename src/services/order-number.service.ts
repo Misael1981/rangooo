68bda33e7d6deae887d89442f0cefd2e3f438ad5
Misael <misaelborges1981@gmail.com>
2026-05-08 10:25:47 -0300

    Correção para o número do pedido

--- src/services/order-number.service.ts
+++ src/services/order-number.service.ts
@@ -1,13 +1,18 @@
 import { Prisma } from "@misael1981/rangooo-database"
+import { toZonedTime } from "date-fns-tz"
 
 export async function generateOrderNumber(
   tx: Prisma.TransactionClient,
   restaurantId: string,
 ) {
-  const startOfDay = new Date()
+  const timeZone = "America/Sao_Paulo"
+
+  const now = toZonedTime(new Date(), timeZone)
+
+  const startOfDay = new Date(now)
   startOfDay.setHours(0, 0, 0, 0)
 
-  const currentDay = new Date().getDate()
+  const currentDay = now.getDate()
 
   const ordersToday = await tx.order.count({
     where: {
